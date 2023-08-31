e2aad45049efe23b34ebb43326d9648254ce03d4
ANSHIKA MISHRA <94507269+anshika0912@users.noreply.github.com>
2023-08-31 14:41:01 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,17 +1,17 @@
-import random
-import string
+import random  # Importing the 'random' module to generate random values.
+import string # Importing the 'string' module to access character sets like ascii_letters, digits, and punctuation.
 
-print("Password Generator")
+print("Password Generator") # Printing a message to indicate the start of the password generator.
 
-length = int(input("Enter the length of the password: "))
+length = int(input("Enter the length of the password: "))  # Taking user input for the desired length of the password.
 
-if length <= 0:
+if length <= 0: # Checking if the entered length is a valid positive number.
     print("Password length must be a positive number.")
 else:
-    password = ''
-    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''     # Initializing an empty string to store the generated password.
+    characters = string.ascii_letters + string.digits + string.punctuation   # Creating a character set by combining letters (both cases), digits, and punctuation.
 
-    for _ in range(length):
+    for _ in range(length):   # Generating the password by randomly selecting characters from the 'characters' set.
         password += random.choice(characters)
 
     print("Generated Password:", password)
